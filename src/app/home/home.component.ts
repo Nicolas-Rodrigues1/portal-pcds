c6910ac30caa749ebddce31493412e14bccd14ae
Nicolas-Rodrigues1 <nicolas.rodrigues@ds2u.com.br>
2024-07-16 12:03:17 -0300

    Formulario pedido no console.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HomeService } from './services/home.service';
 
@@ -28,4 +28,9 @@ export class HomeComponent implements OnInit{
 
     this.homeService.setHomeForm(this.homeForm);
   }
+
+  realizarPedido(){
+    console.log(this.homeForm.value)
+  }
+
 }
